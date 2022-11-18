379e374de5eee5232ca136bc85d389a2c5e32bc3
suma bimantoro andru <suma.andru@gmail.com>
2022-11-18 21:46:43 +0700

    bulit auto ml

---
+++ autoML/app1.py
@@ -0,0 +1,34 @@
+from operator import index
+import streamlit as st
+import plotly.express as px
+# from pycaret.regression import setup, compare_models, pull, save_model, load_model
+# from pycaret.classification import *
+import pandas_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import os 
+
+if os.path.exists('./dataset.csv'): 
+    df = pd.read_csv('dataset.csv', index_col=None)
+
+with st.sidebar: 
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("Auto Suma ML")
+    choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
+    st.info("This project application helps you build and explore your data.")
+
+if choice == "Upload":
+    st.title("Upload Your Dataset")
+    file = st.file_uploader("Upload Your Dataset")
+    if file: 
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == "Profiling": 
+    st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
+
+if choice == "Modelling": 
+    pass
